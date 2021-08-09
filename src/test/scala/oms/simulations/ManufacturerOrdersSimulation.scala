489abde5151229d67714b27d24d4c9e83e684401
Kevin Morson <kevinmorson@Kevins-MacBook-Pro.local>
2021-08-09 11:54:54 +0100

    approve and reject order journeys added

--- src/test/scala/oms/simulations/ManufacturerOrdersSimulation.scala
+++ src/test/scala/oms/simulations/ManufacturerOrdersSimulation.scala
@@ -71,7 +71,7 @@ class ManufacturerOrdersSimulation extends Simulation {
       .check(css("h2:contains('Create Order - Products')").exists)
       .check(status.is(200)))
 
-    .exec(http("post confirm submit order")
+    .exec(http("confirm submit order")
       .post("/fsOrderInput")
       .formParam("""reqaction""", """confirmSubmitOrder""")
       .formParam("""organisationGroup""", manufacturerOrder.deliveryTo)
@@ -211,8 +211,18 @@ class ManufacturerOrdersSimulation extends Simulation {
       .formParam("""productDropdown0""", manufacturerEdit.productDropdown)
       .formParam("""multiple0""", manufacturerEdit.multiple)
       .formParam("""orderNumber""", manufacturerEdit.orderNumber)
+            .check(bodyString.saveAs("RESPONSE_DATA"))
       .check(css("h2:contains('Edit Order - Order Confirmation')").exists)
       .check(status.is(200)))
+
+    .exec(
+      session => {
+        // THIS SECTION IS USED FOR DEBUGGING
+        //        println(s"AUTH TOKEN = ${session("authToken").as[String]}") // prints auth token
+               println(session("RESPONSE_DATA").as[String]) // prints response
+        session
+      }
+    )
 
     .exec(http("confirmed edited order")
       .post("/fsOrderEdit")
@@ -241,6 +251,105 @@ class ManufacturerOrdersSimulation extends Simulation {
       .formParam("""selectedLinkedOrderLineId0""", manufacturerEdit.selectLinkedOrderLineId)
       .check(css("h2:contains('Edit Order - Order Amendment Complete')").exists)
       .check(status.is(200)))
+
+  val approveOrderScenario = scenario("approve order")
+    .exec(http("get login")
+      .get("/index.jsp?timeout=true")
+      .check(status.is(200)))
+
+    .exec(http("input credentials")
+      .post("/fsLogin")
+      .check(headerRegex("Set-Cookie", "(.*)").saveAs("authToken"))
+      .formParam("""userid""", Constants.username)
+      .formParam("""userpassword""", Constants.password)
+      .formParam("""actionrec""", """""")
+      .formParam("""reqaction""", """""")
+      .formParam("""languageIdSelected""", """1""")
+      .formParam("""isLanguageChanged""", """true""")
+      .formParam("""as_sfid""", """AAAAAAW5wqSJNRJZhWsdrXIeeDP2x5dy3lWiMLIvjB5ZyJkMeuEXM4Dkdj6bmv_q4tSovydMFki8Jnv00IK0qn0VNRm6VFsrShUeMK6TgceUyFifPrdI0kCMRU3TnQqt92lFN3sy5egAeQ9tFL0INJsO-2p1F_9pdEeJa93SnqxGzUBHsA==""")
+      .formParam("""as_fid""", """9380625ce296d688ace1d9b5b5be88a72b0f9b01""")
+      .check(status.is(200)))
+
+    .exec(http("get manage orders")
+      .get("/fsHome?reference=L1_Data_Entry")
+      .check(status.is(200)))
+
+    .exec(http("get customer orders")
+      .get("/fsHome?reference=CustomerOrders")
+      .check(status.is(200)))
+
+    .exec(http("get approve order")
+      .get("/fsOrderApprove?reference=DestroyOrderApproval")
+      .check(css("h2:contains('New Orders - Review and Approve')").exists)
+      .check(status.is(200)))
+
+    .exec(http("approve order")
+      .post( "/fsOrderApprove")
+      .formParam("""reqaction""", """approveOrder""")
+      .formParam("""thisPage""", """/orders/orderApprove.jsp""")
+      .formParam("""orderID""", """45""")
+      .formParam("""showWarehouse""", """false""")
+      .formParam("""isLicOrder""", """true""")
+      .formParam("""fulfillerDefaultValue""", """""")
+      .formParam("""isTaxPaymentFeatureOn""", """false""")
+      .formParam("""fulfiller""", """DLR""")
+      .formParam("""bondedWarehouse""", """""")
+      .check(css("h2:contains('Approve Order - Order Approval Complete')").exists)
+      .check(status.is(200)))
+
+  val rejectOrderScenario = scenario("reject order")
+    .exec(http("get login")
+      .get("/index.jsp?timeout=true")
+      .check(status.is(200)))
+
+    .exec(http("input credentials")
+      .post("/fsLogin")
+      .check(headerRegex("Set-Cookie", "(.*)").saveAs("authToken"))
+      .formParam("""userid""", Constants.username)
+      .formParam("""userpassword""", Constants.password)
+      .formParam("""actionrec""", """""")
+      .formParam("""reqaction""", """""")
+      .formParam("""languageIdSelected""", """1""")
+      .formParam("""isLanguageChanged""", """true""")
+      .formParam("""as_sfid""", """AAAAAAW5wqSJNRJZhWsdrXIeeDP2x5dy3lWiMLIvjB5ZyJkMeuEXM4Dkdj6bmv_q4tSovydMFki8Jnv00IK0qn0VNRm6VFsrShUeMK6TgceUyFifPrdI0kCMRU3TnQqt92lFN3sy5egAeQ9tFL0INJsO-2p1F_9pdEeJa93SnqxGzUBHsA==""")
+      .formParam("""as_fid""", """9380625ce296d688ace1d9b5b5be88a72b0f9b01""")
+      .check(status.is(200)))
+
+    .exec(http("get manage orders")
+      .get("/fsHome?reference=L1_Data_Entry")
+      .check(status.is(200)))
+
+    .exec(http("get customer orders")
+      .get("/fsHome?reference=CustomerOrders")
+      .check(status.is(200)))
+
+    .exec(http("get reject order")
+      .get("/fsOrderApprove?reference=DestroyOrderApproval")
+      .check(css("h2:contains('New Orders - Review and Approve')").exists)
+      .check(status.is(200)))
+
+    .exec(http("cancel order")
+      .post( "/fsOrderCancel")
+      .formParam("""reqaction""", """selectOrder""")
+      .formParam("""originatingFrom""", """reject""")
+      .formParam("""selectedorder""", """27""")
+      .formParam("""rejectIsTaxPaid""", """""")
+      .formParam("""rejectTaxPaymentDate""", """""")
+      .formParam("""rejectTaxPaymentAdditionalInfo""", """""")
+      .check(css("h2:contains('Reject Order')").exists)
+      .check(status.is(200)))
+
+    .exec(http("confirm cancel order")
+      .post( "/fsOrderCancel")
+      .formParam("""reqaction""", """selectOrder""")
+      .formParam("""originatingFrom""", """reject""")
+      .formParam("""selectedorder""", """27""")
+      .formParam("""rejectIsTaxPaid""", """""")
+      .formParam("""rejectTaxPaymentDate""", """""")
+      .formParam("""rejectTaxPaymentAdditionalInfo""", """""")
+      .formParam("""comments""", """""")
+      .check(css("h2:contains('New Orders - Review and Approve')").exists)
+      .check(status.is(200)))
     //      // THIS IS HOW TO GRAB RESPONSE FOR DEBUG
     //      //      .check(bodyString.saveAs("RESPONSE_DATA"))
     .exec(
@@ -253,4 +362,10 @@ class ManufacturerOrdersSimulation extends Simulation {
     )
   setUp(createOrderScenario.inject(atOnceUsers(1)).protocols(httpProtocol)
     .andThen(editOrderScenario.inject(atOnceUsers(1)).protocols(httpProtocol)))
+      //.andThen(approveOrderScenario.inject(atOnceUsers(1)).protocols(httpProtocol))
+        //.andThen(rejectOrderScenario.inject(atOnceUsers(1)).protocols(httpProtocol)))
+
+
+
+
 }
