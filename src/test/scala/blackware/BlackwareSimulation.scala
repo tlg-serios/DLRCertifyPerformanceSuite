9ed88ba3a337c6faac52fa196313d56e560e09bb
tlg-serios <77745692+tlg-serios@users.noreply.github.com>
2021-08-10 19:59:40 +0100

    blackware done - needs tidy, only tested with multiples of 1

--- src/test/scala/blackware/BlackwareSimulation.scala
+++ src/test/scala/blackware/BlackwareSimulation.scala
@@ -32,10 +32,12 @@ class BlackwareSimulation extends Simulation {
   var workOrdersToAssign: util.ArrayList[String] = new util.ArrayList[String]
   // Stores work orders
   var workOrdersForContainerCapture: util.ArrayList[String] = new util.ArrayList[String]
+  var workOrdersForDebugging: util.ArrayList[String] = new util.ArrayList[String]
   // Stores work orders
   var workOrdersToPrint: util.ArrayList[String] = new util.ArrayList[String]
   // Stores container numbers to be printed
   var containersToPrint: util.ArrayList[String] = new util.ArrayList[String]
+  var containersForStock: util.ArrayList[String] = new util.ArrayList[String]
   val workOrders: Option[util.ArrayList[WorkOrder]] = Some(new util.ArrayList[WorkOrder]())
 
   // Represents a work orders = a work order reference plus list of associated containers
@@ -53,6 +55,7 @@ class BlackwareSimulation extends Simulation {
   def assignWorkOrder = {
     val workOrder = workOrdersToAssign.get(0)
     workOrdersForContainerCapture.add(workOrder)
+    workOrdersForDebugging.add(workOrder)
     workOrdersToAssign.remove(0)
     workOrder
   }
@@ -62,6 +65,14 @@ class BlackwareSimulation extends Simulation {
     println("CONTAINER NUMBERS" + containersToPrint)
     val container = containersToPrint.get(0)
     containersToPrint.remove(0)
+    container
+  }
+
+  // Returns the next container reference to be printed and then removes it from the list
+  def getNextReadyContainer = {
+    println("CONTAINER NUMBERS" + containersForStock)
+    val container = containersForStock.get(0)
+    containersForStock.remove(0)
     container
   }
 
@@ -225,6 +236,34 @@ class BlackwareSimulation extends Simulation {
     "</UpdateJobContainerStatus>\n" +
     "</s:Body>\n" +
     "</s:Envelope>"
+  val readyForStockString = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+    "<s:Header>\n" +
+    "<h:Butterfly2013Header xmlns:h=\"http://schemas.delarue.com/2013/02/21/Butterfly.2013Header\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+    "<h:RequestID>${uniqueUUID}</h:RequestID>\n" +
+    "<h:ContractID>1</h:ContractID>\n" +
+    "<h:OperatorID>Planning User</h:OperatorID>\n" +
+    "<h:MachineID>3</h:MachineID>\n" +
+    "<h:DateTime>" + "2021-07-12" + "T00:00.0000000Z</h:DateTime>\n" +
+    "<h:AuthenticationToken>F0C4B20D-694A-48F7-8D6A-E7B8D8D19AC2</h:AuthenticationToken>\n" +
+    "<h:Hash i:nil=\"true\"/>\n" +
+    "<h:ThrowConfigurationErrors>false</h:ThrowConfigurationErrors>\n" +
+    "<h:CallingApplicationId i:nil=\"true\"/>\n" +
+    "</h:Butterfly2013Header>\n" +
+    "</s:Header>\n" +
+    "<s:Body>\n" +
+    "<UpdateJobContainerStatus xmlns=\"http://schemas.delarue.com/2013/02/21/Butterfly.UCodeBroker.ServiceContract\">\n" +
+    "<Request xmlns:a=\"http://schemas.delarue.com/2013/02/21/Butterfly.UCodeBroker.DataContract\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+    "<a:ContainerNumber>${interpolateMe}</a:ContainerNumber>\n" +
+    "<a:JobContainerStatus>ReadyForStock</a:JobContainerStatus>\n" +
+    "<a:IsManualStatusChange>true</a:IsManualStatusChange>\n" +
+    "<a:ProductionRunOperatorID>Planning User</a:ProductionRunOperatorID>\n" +
+    "<a:ReasonRequest i:nil=\"true\"></a:ReasonRequest>\n" +
+    "<a:VoidedRequest i:nil=\"true\"></a:VoidedRequest>\n" +
+    "<a:OverrideStatusDateTime>0001-01-01T00:00:00</a:OverrideStatusDateTime>\n" +
+    "</Request>\n" +
+    "</UpdateJobContainerStatus>\n" +
+    "</s:Body>\n" +
+    "</s:Envelope>"
   // Standard protocol
   val httpProtocol: HttpProtocolBuilder = http
     .baseUrl(Constants.blackwareUrl)
@@ -245,7 +284,7 @@ class BlackwareSimulation extends Simulation {
         println("BOOLEAN ADDED")
         allocatedWorkOrders.add(true)
         numberOfWorkOrders = allocatedWorkOrders.size()
-        println(s"number of work orders = ${numberOfWorkOrders}")
+        println(allocatedWorkOrders)
         session
       }
     )
@@ -260,7 +299,6 @@ class BlackwareSimulation extends Simulation {
       .check(status.is(200)))
       .exec(
         session => {
-          println("GET WORK ORDER DETAIL EXECUTED") // prints response
           workOrdersToAssign = workOrderRefs
           session
         })
@@ -283,42 +321,24 @@ class BlackwareSimulation extends Simulation {
       .check(bodyString.saveAs("RESPONSE_DATA"))
       .check(status.is(200)))
       .exec(session => {
-        println(session("RESPONSE_DATA").as[String])
-        println("ONE\n\n\n\n\n\n\n\n\nONE\n\n\n\n\n")
-        println("TWO\n\n\n\n\n\nTWO\n\n\n\n\n")
-        println("THREE\n\n\n\n\n\nTHREE\n\n\n\n\n")
         val container: util.ArrayList[String] = new util.ArrayList[String]
         val x = session("RESPONSE_DATA").as[String]
         val matcher = containerNumberPattern.matcher(x)
-        println("FOUR\n\n\n\n\n\nFOUR\n\n\n\n\n")
-        println(session("RESPONSE_DATA").as[String])
         // TODO could possibly make helper method if needed in future API tests
         while (matcher.find()) {
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
-          println("FROM MATCHER" + matcher.group())
           container.add(matcher.group()
             .replace("ContainerNumber>", "").replace("</b", ""))
         }
-        println("FIVE\n\n\n\n\n\nFIVE\n\n\n\n\n")
         container.forEach(x => {
-          println("CONTAINER ====" + x)
           jobContainers.add(x)
         })
-//        println("\n\n\n\n\n\n\nWork Order = " + workOrders.getOrElse(new util.ArrayList[WorkOrder]()).get(0).containers.get(0))
-        println(s"JOB CONTAINERS SIZE = ${jobContainers.size()}")
+        //        println("\n\n\n\n\n\n\nWork Order = " + workOrders.getOrElse(new util.ArrayList[WorkOrder]()).get(0).containers.get(0))
         jobContainers.forEach(x => {
           if (x.length > 2 && x.length < 13) { //TODO use better conditional
             containersToPrint.add(x)
-            println(containersToPrint)
+            containersForStock.add(x)
           }
         })
-        println(s"CONTAINERS TO PRINT SIZE = ${containersToPrint.size()}")
         numberOfContainers = containersToPrint.size()
         session
       })
@@ -335,10 +355,34 @@ class BlackwareSimulation extends Simulation {
         ).replace("${uniqueUUID}", getUUID)))
       .check(bodyString.saveAs("RESPONSE_DATA"))
       .check(status.is(200)))
+//      .exec(
+//        session => {
+//          println("WORK ORDERS ======")
+//          println(session("RESPONSE_DATA").as[String])
+//          // THIS SECTION IS USED FOR DEBUGGING
+//          //        println(s"AUTH TOKEN = ${session("authToken").as[String]}") // prints auth token
+//          //        println(session("RESPONSE_DATA").as[String]) // prints response
+//          session
+//        }
+//      )
+  }
+
+
+  // session => ... calls are greyed out but are necesarry. Do not remove.
+  def setStampsReadyForStock: ScenarioBuilder = scenario("set stamps ready for stock").repeat(session => numberOfContainers, "containers in job") {
+    exec(http("set stamps printed")
+      .post("/ProductionDataService.svc")
+      .header("SOAPAction", """http://schemas.delarue.com/2013/02/21/Butterfly.UCodeBroker.ServiceContract/IProductionDataService/UpdateJobContainerStatus""")
+      .body(
+        StringBody(session => readyForStockString.replace(
+          "${interpolateMe}", getNextReadyContainer
+        ).replace("${uniqueUUID}", getUUID)))
+      .check(bodyString.saveAs("RESPONSE_DATA"))
+      .check(status.is(200)))
       .exec(
         session => {
-          println("WORK ORDERS ======")
-          println(session("RESPONSE_DATA").as[String])
+          println("WORK ORDERS ======" + workOrdersForDebugging)
+//          println(session("RESPONSE_DATA").as[String])
           // THIS SECTION IS USED FOR DEBUGGING
           //        println(s"AUTH TOKEN = ${session("authToken").as[String]}") // prints auth token
           //        println(session("RESPONSE_DATA").as[String]) // prints response
@@ -350,11 +394,10 @@ class BlackwareSimulation extends Simulation {
   // Set up with andThen() and nothingFor() to ensure consecutive execution TODO refine timings
   // IMPORTANT: andThen calls must ALL be nested for consecutive execution
   // IMPORTANT: At once users is used here to set the number of SKUs to be created, not users at once
-  setUp(
-    allocateUCodeRange.inject(atOnceUsers(7)).protocols(httpProtocol)
+  setUp(allocateUCodeRange.inject(atOnceUsers(7)).protocols(httpProtocol)
       .andThen(getWorkOrderDetail.inject(nothingFor(200.seconds), atOnceUsers(1)).protocols(httpProtocol)
         .andThen(assignStamps.inject(nothingFor(200.seconds), atOnceUsers(1)).protocols(httpProtocol)
           .andThen(getWorkOrderDetailCaptureContainer.inject(nothingFor(400.seconds), atOnceUsers(1)).protocols(httpProtocol)
-            .andThen(setStampsPrinted.inject(nothingFor(200.seconds), atOnceUsers(1)).protocols(httpProtocol)))))
-  )
+            .andThen(setStampsPrinted.inject(nothingFor(200.seconds), atOnceUsers(1)).protocols(httpProtocol)
+              .andThen(setStampsReadyForStock.inject(nothingFor(200.seconds), atOnceUsers(1)).protocols(httpProtocol)))))))
 }
