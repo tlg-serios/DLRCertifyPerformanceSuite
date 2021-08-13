b9e6b32b66c2693ae85ddf6ab5740db555e751f0
tlg-serios <77745692+tlg-serios@users.noreply.github.com>
2021-08-13 09:32:57 +0100

    commit to change branch

--- src/test/scala/oms/simulations/Sandbox.scala
+++ src/test/scala/oms/simulations/Sandbox.scala
@@ -1,46 +1,34 @@
 package oms.simulations
 
-import org.junit.Test
-
-import java.sql.{Connection, DriverManager, SQLException}
+import org.junit._
+import java.sql.DriverManager
+import java.sql.Connection
 
 class Sandbox {
+  val url = """jdbc:sqlserver://SGBBKA6486\APP;databaseName=Butterfly.DAS"""
+//  val driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+  val sqlQuery = "SELECT user_uuid FROM users where user_id = '${userId}'"
+  val username = """DLR\ADM_GreestT"""
+  val password = "%Y4puDt==%xe2C5h"
+  var connection:Connection = null
 
-  val DB_URL = "jdbc:mysql://localhost/TUTORIALSPOINT"
-  val USER = "guest"
-  val PASS = "guest123"
-  val QUERY = "SELECT id, first, last, age FROM Employees"
+  @Test def foo = {
+    try {
+      // make the connection
+      Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+      connection = DriverManager.getConnection(url, username, password)
 
-  def db = {
-      // connect to the database named "mysql" on the localhost
-      val driver = "com.mysql.jdbc.Driver"
-      val url = "jdbc:sqlserver://SGBBKA6486//APP"
-      val username = "adm/greestt"
-      val password = "%Y4puDt==%xe2C5h"
-
-      // there's probably a better way to do this
-      var connection:Connection = null
-
-      try {
-        // make the connection
-        Class.forName(driver)
-        connection = DriverManager.getConnection(url, username, password)
-
-        // create the statement, and run the select query
-        val statement = connection.createStatement()
-        val resultSet = statement.executeQuery("SELECT host, user FROM user")
-        while ( resultSet.next() ) {
-          val host = resultSet.getString("host")
-          val user = resultSet.getString("user")
-          println("host, user = " + host + ", " + user)
-        }
-      } catch {
-        case e => e.printStackTrace
+      // create the statement, and run the select query
+      val statement = connection.createStatement()
+      val resultSet = statement.executeQuery("SELECT host, user FROM user")
+      while ( resultSet.next() ) {
+        val host = resultSet.getString("host")
+        val user = resultSet.getString("user")
+        println("host, user = " + host + ", " + user)
       }
-      connection.close()
-  }
-
-  @Test def dbTest = {
-    db
+    } catch {
+      case e => e.printStackTrace
+    }
+    connection.close()
   }
 }
