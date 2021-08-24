8637a3b0858d9f9dfc6bf1fee4b0efa24ee5b652
tlg-serios <77745692+tlg-serios@users.noreply.github.com>
2021-08-24 10:09:14 +0100

    added auto2 urls

--- src/test/scala/oms/Constants.scala
+++ src/test/scala/oms/Constants.scala
@@ -1,8 +1,8 @@
 package oms
 
 object Constants {
-  val baseUrl = "https://demo.dlrcertify.com" + "/auto1" // make variable
-  val blackwareUrl = "http://sgbbka6485:9021" // make variable
+  val baseUrl = "https://demo.dlrcertify.com" + "/auto2" // make variable
+  val blackwareUrl = "http://sgbbka6495:9021" // make variable
   val username = "ISAdmin"
   val password = "T@x5t@mp5!!"
   val orderRef = "perf124"
