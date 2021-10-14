34644ae3fcc9d52706ed9e07bdfa2a814bf640ec
tlg-serios <77745692+tlg-serios@users.noreply.github.com>
2021-10-14 14:49:42 +0100

    commit to change branch

--- src/test/scala/EPCIS/EPCISEventSimulation.scala
+++ src/test/scala/EPCIS/EPCISEventSimulation.scala
@@ -2,6 +2,7 @@ package EPCIS
 
 //import EPCIS.CSVHandler
 //import EPCIS.{DPCHandler, EventHandler}
+
 import io.gatling.core.Predef._
 import io.gatling.core.feeder.FeederBuilderBase
 import io.gatling.core.structure.ScenarioBuilder
@@ -35,7 +36,7 @@ class EPCISEventSimulation extends Simulation {
   }
 
 
-  val sagaEPCISData : FeederBuilderBase[String]#F = Array(
+  val sagaEPCISData: FeederBuilderBase[String]#F = Array(
     Map("date123" -> getTimestamp, "readPoint" -> "urn:epc:id:sgln:7311101.00001.99MACH1", "sourceLocation" -> "urn:epc:id:gdti:7311101.00001.00", "sourceOwner" -> "urn:epc:id:sgln:7311101.00000.00", "destOwner" -> "urn:epc:id:sgln:7311101.00000.00", "destLocation" -> "urn:epc:id:sgln:7311101.00000.00", "bizLocation" -> "urn:epc:id:gdti:7311101.00001.00", "bizTrans" -> "urn:epcglobal:cbv:bt:7311101.00001.00", "epcCommissionPrefix" -> "http://dts.gta.gov.qa/epcis-aggr/obj/")).circular
 
 
@@ -44,49 +45,56 @@ class EPCISEventSimulation extends Simulation {
     .inferHtmlResources()
     .acceptHeader("text/html;charset=UTF-8")
     .header("Content-Type", "text/xml; charset=utf-8")
-    .header("username", "admin")
-    .header("password", "Staffanstorp!")
+    .header("Authorization", "Basic YWRtaW46U3RhZmZhbnN0b3JwIQ==")
 
   def callEpcis: ScenarioBuilder = scenario("callEPCIS").feed(sagaEPCISData)
+
   exec(http("call epcis")
     .post("/api/acquire/rabbitmq/epcis")
     .body(StringBody("commissionString"))
-//    .body(StringBody(session => generateCommissionEvent(getDPCs(5).asScala.toList)))
+    //    .body(StringBody(session => generateCommissionEvent(getDPCs(5).asScala.toList)))
     .check(bodyString.saveAs("RESPONSE_DATA"))
     .check(status.is(200))).pause(1)
 
 
-  val scnT : ScenarioBuilder = scenario("Scenario1").feed(sagaEPCISData)
+  val scnT: ScenarioBuilder = scenario("Scenario1").feed(sagaEPCISData)
     .exec(http("get entitlement")
-      .post("/DpcDownload/DpcDownloadService.svc")
+      .post("/api/acquire/rabbitmq/epcis")
       .body(StringBody(session => generateCommissionEvent(getDPCs(5).asScala.toList)))
       .check(bodyString.saveAs("RESPONSE_DATA"))
       .check(status.is(200)))
+    .exec(
+      session => {
+        println(session("RESPONSE_DATA").as[String])
+        session
+      }
+    )
 
   def callEpcis2: ScenarioBuilder = scenario("callEPCIS").feed(sagaEPCISData)
+
   exec(http("call epcis")
     .post("/api/acquire/rabbitmq/epcis")
     .body(StringBody("commissionString")))
 
-//  @Test def generate() = {
-//    seperateDPCS("C:" + File.separator + "Workspace" + File.separator +
-//      "certify-performance-tests" + File.separator + "src" + File.separator + "test" + File.separator + "scala" + File.separator +"EPCIS" + File.separator + "A1Not applicable_0001C_2021_10_06_11_39_AM.csv")
-//    println(generateCommissionEvent(getDPCs(5).asScala.toList))
-//    println(generateCommissionEvent(getDPCs(5).asScala.toList))
-//    println(generateCommissionEvent(getDPCs(5).asScala.toList))
-//  }
-//
-//  @Test def generateDPCList() = {
-//    seperateDPCS("C:" + File.separator + "Workspace" + File.separator +
-//      "certify-performance-tests" + File.separator + "src" + File.separator + "test" + File.separator + "scala" + File.separator +"EPCIS" + File.separator + "A1Not applicable_0001C_2021_10_06_11_39_AM.csv")
-//    println(getDPCs(5))
-//    println(getDPCs(4))
-//    println(getDPCs(8))
-//    println(getDPCs(7))
-//  }
+  //  @Test def generate() = {
+  //    seperateDPCS("C:" + File.separator + "Workspace" + File.separator +
+  //      "certify-performance-tests" + File.separator + "src" + File.separator + "test" + File.separator + "scala" + File.separator +"EPCIS" + File.separator + "A1Not applicable_0001C_2021_10_06_11_39_AM.csv")
+  //    println(generateCommissionEvent(getDPCs(5).asScala.toList))
+  //    println(generateCommissionEvent(getDPCs(5).asScala.toList))
+  //    println(generateCommissionEvent(getDPCs(5).asScala.toList))
+  //  }
+  //
+  //  @Test def generateDPCList() = {
+  //    seperateDPCS("C:" + File.separator + "Workspace" + File.separator +
+  //      "certify-performance-tests" + File.separator + "src" + File.separator + "test" + File.separator + "scala" + File.separator +"EPCIS" + File.separator + "A1Not applicable_0001C_2021_10_06_11_39_AM.csv")
+  //    println(getDPCs(5))
+  //    println(getDPCs(4))
+  //    println(getDPCs(8))
+  //    println(getDPCs(7))
+  //  }
 
   before(seperateDPCS("C:" + File.separator + "Workspace" + File.separator +
-      "certify-performance-tests" + File.separator + "src" + File.separator + "test" + File.separator + "scala" + File.separator +"EPCIS" + File.separator + "A1Not applicable_0001C_2021_10_06_11_39_AM.csv"))
+    "certify-performance-tests" + File.separator + "src" + File.separator + "test" + File.separator + "scala" + File.separator + "EPCIS" + File.separator + "A1Not applicable_0001C_2021_10_06_11_39_AM.csv"))
 
   def seperateDPCS(fileName: String) = {
     for (line <- Source.fromFile(fileName).getLines) {
@@ -121,22 +129,22 @@ class EPCISEventSimulation extends Simulation {
   var dpcsFromCSV: util.ArrayList[String] = new util.ArrayList[String]()
   var partitionedDPCs: util.ArrayList[List[String]] = new util.ArrayList[List[String]]()
 
-//  var separatedDPCs = new util.TreeMap[String, util.ArrayList[String]]()
-//  var liveDPCs: util.ArrayList[String] = new util.ArrayList[String]()
-//  var wasteDPCs: util.ArrayList[String]  = new util.ArrayList[String]()
+  //  var separatedDPCs = new util.TreeMap[String, util.ArrayList[String]]()
+  //  var liveDPCs: util.ArrayList[String] = new util.ArrayList[String]()
+  //  var wasteDPCs: util.ArrayList[String]  = new util.ArrayList[String]()
 
-//  var strings = new util.ArrayList[String]
+  //  var strings = new util.ArrayList[String]
 
   def generateCommissionEvent(dpcs: List[String]) = {
-//    seperateDPCS("C:\\Workspace\\certify-performance-tests\\src\\test\\scala\\EPCIS\\A1Not applicable_0001C_2021_10_06_11_39_AM.csv")
+    //    seperateDPCS("C:\\Workspace\\certify-performance-tests\\src\\test\\scala\\EPCIS\\A1Not applicable_0001C_2021_10_06_11_39_AM.csv")
     var epcList = ""
     dpcs.foreach(dpc => {
       epcList = epcList +
-      "<epc>EPCCOMMISSIONPREFIX" + dpc + "</epc>\n"
+        "<epc>EPCCOMMISSIONPREFIX" + dpc + "</epc>\n"
     })
     commissionString.replace("EPC_LIST", epcList)
       .replace("INSTANCE_ID", getInstanceID.toString)
-      .replace("DATE", getTimestamp)
+      .replace("DATE", "2021-10-14" + """T00:00:00.000Z""")
       .replace("EPCCOMMISSIONPREFIX", "http://dts.gta.gov.qa/epcis-aggr/obj/")
       .replace("READPOINT", "urn:epc:id:sgln:7311101.00001.99MACH1")
       .replace("BIZLOCATION", "urn:epc:id:gdti:7311101.00001.00")
@@ -154,7 +162,7 @@ class EPCISEventSimulation extends Simulation {
     var epcList = ""
     dpcs.foreach(dpc => {
       epcList = epcList +
-      "<epc>${epcAggregationPrefix}" + dpc + "</epc>\n"
+        "<epc>${epcAggregationPrefix}" + dpc + "</epc>\n"
     })
     aggregationString.replace("EPC_LIST", epcList).replace("INSTANCE_ID", getInstanceID.toString)
   }
@@ -163,13 +171,14 @@ class EPCISEventSimulation extends Simulation {
     var epcList = ""
     dpcs.foreach(dpc => {
       epcList = epcList +
-      "<epc>${epcCommissionPrefix}" + dpc + "</epc>\n"
+        "<epc>${epcCommissionPrefix}" + dpc + "</epc>\n"
     })
     shippingString.replace("EPC_LIST", epcList).replace("INSTANCE_ID", getInstanceID.toString)
   }
 
 
-  val epcisString = """<?xml version="1.0" encoding="utf-8"?>
+  val epcisString =
+    """<?xml version="1.0" encoding="utf-8"?>
     <epcis:EPCISDocument xmlns:fcc="http://fracturecode.com" schemaVersion="1.2" creationDate="2017-10-27T13:45:03.064+00:00" xmlns:epcis="urn:epcglobal:epcis:xsd:1">
       <EPCISHeader>
         <StandardBusinessDocumentHeader xmlns="http://www.unece.org/cefact/namespaces/StandardBusinessDocumentHeader">
@@ -258,38 +267,38 @@ class EPCISEventSimulation extends Simulation {
     </ObjectEvent>"""
   )
 
-//  var getNextString = {
-//    val string = strings.get(0)
-//    strings.remove(0)
-//    string
-//  }
+  //  var getNextString = {
+  //    val string = strings.get(0)
+  //    strings.remove(0)
+  //    string
+  //  }
 
   def getTimestamp: String = {
     LocalDate.now.toString
   }
 
-//  var calls = new util.ArrayList[String]
+  //  var calls = new util.ArrayList[String]
 
-//  def createStrings(): List[String] = {
-//
-//    List("")
-//  }
-//
-//      api.call(EventHandler.getDestroyXMLStrings(wasteDPCs))
-//
-//  CSVHandler.output("destroyedCodes.csv", separatedDPCs.get("wasteDPCs"))
-//    // create comission events live dpcs and send to saga
-//    api.call(EventHandler.getCommissionXMLStrings(liveDPCs))
-//    CSVHandler.output("commissionedCodes.csv", separatedDPCs.get("liveDPCs"))
-//    // create shipment object with live dpcs
-//    val shipment = EventHandler.getAggregationXMLStrings(liveDPCs)
-//    // send aggregation events for this shipment to saga
-//    api.call(shipment.getAggregationEventStrings)
-//    CSVHandler.output("aggregatedCodes.csv", shipment.getEPCs)
-//    // create shipping event with container id and send to saga
-//    api.call(EventHandler.getShippingXMLString(shipment.getContainerID))
-//    CSVHandler.output("shippedCodes.csv", shipment.getContainerID)
-//  // set data feeder as ArrayList 'refs', then
+  //  def createStrings(): List[String] = {
+  //
+  //    List("")
+  //  }
+  //
+  //      api.call(EventHandler.getDestroyXMLStrings(wasteDPCs))
+  //
+  //  CSVHandler.output("destroyedCodes.csv", separatedDPCs.get("wasteDPCs"))
+  //    // create comission events live dpcs and send to saga
+  //    api.call(EventHandler.getCommissionXMLStrings(liveDPCs))
+  //    CSVHandler.output("commissionedCodes.csv", separatedDPCs.get("liveDPCs"))
+  //    // create shipment object with live dpcs
+  //    val shipment = EventHandler.getAggregationXMLStrings(liveDPCs)
+  //    // send aggregation events for this shipment to saga
+  //    api.call(shipment.getAggregationEventStrings)
+  //    CSVHandler.output("aggregatedCodes.csv", shipment.getEPCs)
+  //    // create shipping event with container id and send to saga
+  //    api.call(EventHandler.getShippingXMLString(shipment.getContainerID))
+  //    CSVHandler.output("shippedCodes.csv", shipment.getContainerID)
+  //  // set data feeder as ArrayList 'refs', then
 
   setUp(scnT.inject(atOnceUsers(1)).protocols(httpProtocol))
 }
